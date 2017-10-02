9be369d3422955ad008c922cec3167062929adf9
Sentyabov Sergey <sergsenta1995@gmail.com>
2017-10-03 00:08:33 +0400

    add base ui

--- main-window.cpp
+++ main-window.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->object_graphicsView->setFixedSize(100,100);
+
+    ui->inventory_tableWidget->setColumnCount(3);
+    ui->inventory_tableWidget->setRowCount(3);
+    ui->inventory_tableWidget->horizontalHeader()->hide();
+    ui->inventory_tableWidget->verticalHeader()->hide();
+    ui->inventory_tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->inventory_tableWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->inventory_tableWidget->setFixedSize(300,300);
 }
 
 MainWindow::~MainWindow()
